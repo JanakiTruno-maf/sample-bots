a80016f0e15b117c49519298f8ab5cf3578f5ae1
JanakiTruno-maf <Janaki.Truno@mafgroup.co.uk>
2025-09-18 23:07:38 +0530

    updated app.py to see if its working

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return {'message': 'Hello from Cloud Run!', 'status': 'success'}
+    return {'message': 'Hello,Hi from Cloud Run!', 'status': 'success'}
 
 @app.route('/health')
 def health():
